47e4fa6c8ae54747b75c9757d6c9a6ced9a1239d
Risdorn <rishabhindoria2203@gmail.com>
2024-06-15 20:39:46 +0530

    Adding file Compression

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@
 import socket
 import threading
 import sys
+import gzip
 
 def request_handler(request_line, header, request_body):
     if request_line[0] == "GET" and request_line[1] == "/":
@@ -12,7 +13,7 @@ def request_handler(request_line, header, request_body):
         if encoding is None or encoding.find("gzip") == -1:
             response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(response)}\r\n\r\n{response}"
         else:
-            response = f"HTTP/1.1 200 OK\r\nContent-Encoding: gzip\r\nContent-Type: text/plain\r\nContent-Length: {len(response)}\r\n\r\n{response}"
+            response = f"HTTP/1.1 200 OK\r\nContent-Encoding: gzip\r\nContent-Type: text/plain\r\nContent-Length: {len(response)}\r\n\r\n{gzip.compress(response.encode('utf-8').decode('utf-8'))}"
     elif request_line[0] == "GET" and request_line[1] == "/user-agent":
         response = header["User-Agent"]
         response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(response)}\r\n\r\n{response}"
