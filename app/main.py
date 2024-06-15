486e5853edc158c7bab9afd24e1b9e0a0b8bf265
Risdorn <rishabhindoria2203@gmail.com>
2024-06-15 20:24:41 +0530

    Adding file Compression

--- app/main.py
+++ app/main.py
@@ -9,6 +9,7 @@ def request_handler(request_line, header, request_body):
     elif request_line[0] == "GET" and request_line[1].startswith("/echo"):
         response = request_line[1].split("/")[-1]
         encoding = header[0].split(": ")[-1]
+        print(encoding)
         if encoding == "gzip":
             response = f"HTTP/1.1 200 OK\r\nContent-Encoding: {encoding}\r\nContent-Type: text/plain\r\nContent-Length: {len(response)}\r\n\r\n{response}"
         else:
